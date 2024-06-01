24c24c715700cf98b27926a37f489910b17ca6ea
Lillian Kao <lillian.kao@kellogg.northwestern.edu>
2024-06-01 02:07:05 +0000

    edits

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user["password"] == params["password"]
+      # login the user
+      redirect_to "/places"
+    else
+      redirect_to "/login
+    if BCrypt::Password.new(@user["password"]) == params["password"]
   end
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,8 @@ ActiveRecord::Schema[7.1].define(version: 2022_03_09_020701) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email"
     t.string "password"
     t.datetime "created_at", null: false
